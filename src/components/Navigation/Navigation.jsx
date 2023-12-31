68cce879ef581e444a68a827329784ab3932b30b
Chirag0812 <chiragzx@gmail.com>
2023-12-31 19:36:57 +0530

    Made deploy Fixes

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,12 @@
 import styles from "./Navigation.module.css";
-import imgg from './abc.png'
+// import imgg from './abc.png'
 //  let imgg= require('../../images/abc.png')
 const Navigation = () => {
   return (
     <nav className={`${styles.navigation} container`}>
       <div>
         {/* <img src={imgg} alt="logo" width="60px" height="70px" /> */}
-        <img src={imgg} alt="logo" width="60px" height="70px" />
+        <img src="./abc.png" alt="logo" width="60px" height="70px" />
       </div>
 
       <ul>
